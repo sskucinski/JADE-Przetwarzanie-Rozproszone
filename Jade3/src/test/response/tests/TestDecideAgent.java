f3b4bf85509844dd7fdc1f6d20bb984e5d7afe92
sskucinski <skut88@hotmail.com>
2012-12-07 11:55:10 +0100

    Testing class update

--- Jade3/src/test/response/tests/TestDecideAgent.java
+++ Jade3/src/test/response/tests/TestDecideAgent.java
@@ -1,5 +1,42 @@
 package test.response.tests;
 
-public class TestDecideAgent {
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.wrapper.AgentController;
+import test.common.Test;
+import test.common.TestException;
+
+public class TestDecideAgent extends Test {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private AgentController responderAgent = null;
+	private static final String RESPONDER_NAME = "responder";
+	private AID responder = null;
+	
+	
+	@Override
+	public Behaviour load(Agent a) throws TestException {
+		setTimeout(1000);
+		
+		String testArg = this.getTestArgument("el");
+		System.out.println("getTestArg: " + testArg);
+		
+		responder = new AID(RESPONDER_NAME, AID.ISLOCALNAME);
+		
+		try {
+			
+			// To-Do...
+			
+			return null;
+			
+		} catch (Exception e) {
+			throw new TestException("Error loading test", e);
+		}
+		
+	}
 
 }
